837d76f998e09a4e58b97ca666caf956f94667ce
mikong <4162+mikong@users.noreply.github.com>
2019-03-03 22:22:45 +0800

    Refactor CBC decrypt

--- w2-aes/src/main.rs
+++ w2-aes/src/main.rs
@@ -5,7 +5,7 @@ use aes::block_cipher_trait::generic_array::GenericArray;
 use aes::block_cipher_trait::BlockCipher;
 use aes::Aes128;
 
-fn cbc_decrypt(key: &[u8], prev_block: &[u8], block: &[u8]) -> Vec<u8> {
+fn cbc_decrypt_block(key: &[u8], prev_block: &[u8], block: &[u8]) -> Vec<u8> {
     let key = GenericArray::from_slice(key);
     let mut buf = GenericArray::clone_from_slice(block);
 
@@ -18,6 +18,19 @@ fn cbc_decrypt(key: &[u8], prev_block: &[u8], block: &[u8]) -> Vec<u8> {
         .collect()
 }
 
+fn cbc_decrypt(key: &[u8], ciphertext: &[u8]) -> Vec<u8> {
+    ciphertext.chunks(16)
+        .collect::<Vec<_>>()
+        .windows(2)
+        .map(|pair| {
+            cbc_decrypt_block(key, pair[0], pair[1])
+        })
+        .flatten()
+        .collect()
+
+    // TODO: remove padding
+}
+
 fn main() {
     let key = hex!("140b41b22a29beb4061bda66b6747e14");
     let ciphertext = hex!("4ca00ff4c898d61e1edbf1800618fb28
@@ -25,16 +38,7 @@ fn main() {
                            2e4b7465d5290d0c0e6c6822236e1daa
                            fb94ffe0c5da05d9476be028ad7c1d81");
 
-    let decoded: Vec<_> = ciphertext.chunks(16)
-        .collect::<Vec<_>>()
-        .windows(2)
-        .map(|pair| {
-            cbc_decrypt(&key, pair[0], pair[1])
-        })
-        .flatten()
-        .collect();
-
-    // TODO: remove padding
+    let decoded = cbc_decrypt(&key, &ciphertext);
 
     println!("{:?}", String::from_utf8_lossy(&decoded));
 }
