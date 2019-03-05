c624188884622a01f8768a7cb692465f62594327
mikong <4162+mikong@users.noreply.github.com>
2019-03-05 15:51:15 +0800

    feat: Read last block of input file and hash it

--- w3-file_auth/src/main.rs
+++ w3-file_auth/src/main.rs
@@ -1,11 +1,26 @@
 use std::env;
-use std::fs;
+use std::fs::File;
+use std::fs::Metadata;
+use std::io;
+use std::io::prelude::*;
+use std::io::SeekFrom;
 use std::path::Path;
 use std::process;
 
 use sha2::{Sha256, Digest};
 
-fn main() -> std::io::Result<()> {
+const KB: u64 = 1024;
+const DEFAULT_BUF_SIZE: usize = 1024;
+
+fn total_blocks(metadata: &Metadata) -> u64 {
+    let size = metadata.len();
+    println!("File size: {}", size);
+
+    let pos = size as f64 / KB as f64;
+    pos.ceil() as u64
+}
+
+fn main() -> io::Result<()> {
     let args: Vec<_> = env::args_os().skip(1).collect();
     if args.len() < 1 {
         println!("Error: File path argument is missing");
@@ -14,13 +29,26 @@ fn main() -> std::io::Result<()> {
     let filename = &args[0];
 
     let path = Path::new(filename);
-    let size = fs::metadata(path)?.len();
+    let mut f = File::open(path)?;
+    let metadata = f.metadata()?;
 
-    println!("File size: {}", size);
+    // Move cursor to last block of the file
+    let pos = (total_blocks(&metadata) - 1) * KB;
+    f.seek(SeekFrom::Start(pos))?;
 
+    // Read last block
+    let mut buf = [0; DEFAULT_BUF_SIZE];
+    let len = match f.read(&mut buf) {
+        Ok(0) => return Ok(()),
+        Ok(len) => len,
+        Err(e) => return Err(e),
+    };
+
+    println!("Block length: {}", len);
+
+    // Hash last block
     let mut hasher = Sha256::new();
-    hasher.input(b"File Auth with SHA256");
-
+    hasher.input(&buf.to_vec());
     let result = hasher.result();
 
     println!("Hash: {:x}", result);
