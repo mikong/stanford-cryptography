39056a8a12fe355a7b502b4a185d411cbb8f0a56
mikong <4162+mikong@users.noreply.github.com>
2019-03-07 14:11:51 +0800

    feat: Create hyper client with a basic GET request

--- w4-padding_oracle_attack/src/main.rs
+++ w4-padding_oracle_attack/src/main.rs
@@ -1,3 +1,24 @@
+extern crate hyper;
+
+use hyper::Client;
+use hyper::rt::{self, Future};
+
 fn main() {
     println!("Padding Oracle Attack!");
+
+    rt::run(rt::lazy(|| {
+
+        let client = Client::new();
+        let uri = "http://crypto-class.appspot.com/po?er=".parse().unwrap();
+
+        client
+            .get(uri)
+            .map(|res| {
+                println!("Response: {}", res.status());
+            })
+            .map_err(|err| {
+                println!("Error: {}", err);
+            })
+
+    }));
 }
