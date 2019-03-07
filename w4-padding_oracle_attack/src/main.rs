ac9377129d030f345ed0e91d839f9e4c8b1f1fce
mikong <4162+mikong@users.noreply.github.com>
2019-03-07 17:43:54 +0800

    feat: Add PaddingOracle struct with reusable client

--- w4-padding_oracle_attack/src/main.rs
+++ w4-padding_oracle_attack/src/main.rs
@@ -1,24 +1,50 @@
 extern crate hyper;
 
-use hyper::Client;
+use hyper::{Body, Client};
+use hyper::client::HttpConnector;
+use hyper::http::Uri;
 use hyper::rt::{self, Future};
+
+const TARGET: &str = "http://crypto-class.appspot.com/po?er=";
+
+#[derive(Debug)]
+struct PaddingOracle {
+    client: Client<HttpConnector, Body>,
+    target_uri: String,
+}
+
+impl PaddingOracle {
+    fn new(target_uri: &str) -> PaddingOracle {
+        let client = Client::new();
+        let target_uri = target_uri.to_string();
+
+        PaddingOracle { client, target_uri }
+    }
+
+    fn get_uri(&self, q: &str) -> Uri {
+        let uri = format!("{}{}", self.target_uri, q);
+        uri.parse::<Uri>().unwrap()
+    }
+
+    fn query(&self, q: &str) -> impl Future<Item=(), Error=()> {
+        let uri = self.get_uri(q);
+
+        self.client
+            .get(uri)
+            .map(|res| {
+                println!("Response: {}", res.status()); 
+            })
+            .map_err(|err| {
+                println!("Error: {}", err);
+            })
+    }
+}
 
 fn main() {
     println!("Padding Oracle Attack!");
 
     rt::run(rt::lazy(|| {
-
-        let client = Client::new();
-        let uri = "http://crypto-class.appspot.com/po?er=".parse().unwrap();
-
-        client
-            .get(uri)
-            .map(|res| {
-                println!("Response: {}", res.status());
-            })
-            .map_err(|err| {
-                println!("Error: {}", err);
-            })
-
+        let po = PaddingOracle::new(TARGET);
+        po.query("")
     }));
 }
