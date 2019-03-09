5b897b09d377de4dafff7976cdea08705e1347ad
mikong <4162+mikong@users.noreply.github.com>
2019-03-09 22:38:15 +0800

    refactor: Move build table and lookup to own functions

--- w5-mitm_dlog/src/main.rs
+++ w5-mitm_dlog/src/main.rs
@@ -4,6 +4,45 @@ use std::collections::HashMap;
 
 use num_bigint::BigUint;
 
+type BigMap = HashMap<BigUint, u32>;
+
+fn build_table(h: &BigUint, g: &BigUint, p: &BigUint, b: u32) -> BigMap {
+    let mut table = HashMap::new();
+
+    let two = vec![2];
+    let two = BigUint::new(two);
+    let p_minus_2 = p - &two;
+
+    for x1 in 0..b {
+        let bytes = x1.to_le_bytes();
+        let big_x1 = BigUint::from_bytes_le(&bytes);
+        let g_x1 = g.modpow(&big_x1, p);
+        let g_x1_inverse = g_x1.modpow(&p_minus_2, p);
+        let left = h * &g_x1_inverse % p;
+
+        table.insert(left, x1);
+    }
+
+    table
+}
+
+fn find_x(table: &BigMap, g: &BigUint, p: &BigUint, b: u32) {
+    let big_b = BigUint::from_bytes_le(&b.to_le_bytes());
+
+    for x0 in 0..b {
+        let g_b = g.modpow(&big_b, p);
+
+        let bytes = x0.to_le_bytes();
+        let big_x0 = BigUint::from_bytes_le(&bytes);
+        let right = g_b.modpow(&big_x0, p);
+
+        if let Some(x1) = table.get(&right) {
+            println!("x0: {}, x1: {}", x0, x1);
+            break;
+        }
+    }
+}
+
 fn main() {
     println!("Meet-in-the-Middle Attack (MITM)");
 
@@ -13,35 +52,8 @@ fn main() {
     let g = BigUint::new(g);
     let p = vec![499];
     let p = BigUint::new(p);
-    let two = vec![2];
-    let two = BigUint::new(two);
-    let p_minus_2 = &p - &two;
-
-    let mut table = HashMap::new();
-
     let b = 2u32.pow(3);
-    let big_b = BigUint::from_bytes_le(&b.to_le_bytes());
 
-    for x1 in 0..b {
-        let bytes = x1.to_le_bytes();
-        let big_x1 = BigUint::from_bytes_le(&bytes);
-        let g_x1 = g.modpow(&big_x1, &p);
-        let g_x1_inverse = g_x1.modpow(&p_minus_2, &p);
-        let left = &h * &g_x1_inverse % &p;
-
-        table.insert(left, x1);
-    }
-
-    for x0 in 0..b {
-        let g_b = g.modpow(&big_b, &p);
-
-        let bytes = x0.to_le_bytes();
-        let big_x0 = BigUint::from_bytes_le(&bytes);        
-        let right = g_b.modpow(&big_x0, &p);
-
-        if let Some(x1) = table.get(&right) {
-            println!("x0: {}, x1: {}", x0, x1);
-            break;
-        }
-    }
+    let table = build_table(&h, &g, &p, b);
+    find_x(&table, &g, &p, b);
 }
