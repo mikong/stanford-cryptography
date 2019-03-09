eae09b0686d5ed9e6aa8c0f5ba0b308201d80554
mikong <4162+mikong@users.noreply.github.com>
2019-03-09 22:12:58 +0800

    feat: Store left in hash table, calculate right and compare Also, update variable names

--- w5-mitm_dlog/src/main.rs
+++ w5-mitm_dlog/src/main.rs
@@ -1,28 +1,47 @@
 extern crate num_bigint;
 
+use std::collections::HashMap;
+
 use num_bigint::BigUint;
 
 fn main() {
     println!("Meet-in-the-Middle Attack (MITM)");
 
-    let hv = vec![491];
-    let h = BigUint::new(hv);
-    let gv = vec![2];
-    let g = BigUint::new(gv);
-    let pv = vec![499];
-    let p = BigUint::new(pv);
+    let h = vec![491];
+    let h = BigUint::new(h);
+    let g = vec![2];
+    let g = BigUint::new(g);
+    let p = vec![499];
+    let p = BigUint::new(p);
     let two = vec![2];
-    let big2 = BigUint::new(two);
-    let mut left: BigUint;
+    let two = BigUint::new(two);
+    let p_minus_2 = &p - &two;
+
+    let mut table = HashMap::new();
 
     let b = 2u32.pow(3);
+    let big_b = BigUint::from_bytes_le(&b.to_le_bytes());
+
     for x1 in 0..b {
         let bytes = x1.to_le_bytes();
         let big_x1 = BigUint::from_bytes_le(&bytes);
-        let gx1 = g.modpow(&big_x1, &p);
-        let p2_exp = &p - &big2;
-        let gnx1 = gx1.modpow(&p2_exp, &p);
-        left = &h * &gnx1 % &p;
-        println!("{:?}", left);
+        let g_x1 = g.modpow(&big_x1, &p);
+        let g_x1_inverse = g_x1.modpow(&p_minus_2, &p);
+        let left = &h * &g_x1_inverse % &p;
+
+        table.insert(left, x1);
+    }
+
+    for x0 in 0..b {
+        let g_b = g.modpow(&big_b, &p);
+
+        let bytes = x0.to_le_bytes();
+        let big_x0 = BigUint::from_bytes_le(&bytes);        
+        let right = g_b.modpow(&big_x0, &p);
+
+        if let Some(x1) = table.get(&right) {
+            println!("x0: {}, x1: {}", x0, x1);
+            break;
+        }
     }
 }
