f6a1e46d143da51960384709e43d538b5ab62f50
mikong <4162+mikong@users.noreply.github.com>
2019-03-10 12:00:57 +0800

    feat: Use actual huge numbers, and optimize: Update dev profile to optimize level 3 Initialize HashMap with known capacity For modular inversion, calculate g^(p-2) ahead of time Loop-invariant code motion for g^B in lookup

--- w5-mitm_dlog/src/main.rs
+++ w5-mitm_dlog/src/main.rs
@@ -7,17 +7,17 @@ use num_bigint::BigUint;
 type BigMap = HashMap<BigUint, u32>;
 
 fn build_table(h: &BigUint, g: &BigUint, p: &BigUint, b: u32) -> BigMap {
-    let mut table = HashMap::new();
+    let mut table = HashMap::with_capacity(b as usize);
 
     let two = vec![2];
     let two = BigUint::new(two);
     let p_minus_2 = p - &two;
+    let g_inverse = g.modpow(&p_minus_2, p);
 
     for x1 in 0..b {
         let bytes = x1.to_le_bytes();
         let big_x1 = BigUint::from_bytes_le(&bytes);
-        let g_x1 = g.modpow(&big_x1, p);
-        let g_x1_inverse = g_x1.modpow(&p_minus_2, p);
+        let g_x1_inverse = g_inverse.modpow(&big_x1, p);
         let left = h * &g_x1_inverse % p;
 
         table.insert(left, x1);
@@ -28,10 +28,9 @@ fn build_table(h: &BigUint, g: &BigUint, p: &BigUint, b: u32) -> BigMap {
 
 fn lookup_x0_x1(table: &BigMap, g: &BigUint, p: &BigUint, b: u32) -> Option<(u32, u32)> {
     let big_b = BigUint::from_bytes_le(&b.to_le_bytes());
+    let g_b = g.modpow(&big_b, p);
 
     for x0 in 0..b {
-        let g_b = g.modpow(&big_b, p);
-
         let bytes = x0.to_le_bytes();
         let big_x0 = BigUint::from_bytes_le(&bytes);
         let right = g_b.modpow(&big_x0, p);
@@ -50,13 +49,10 @@ fn find_x(x0: u32, x1: u32, b: u32) -> u64 {
 fn main() {
     println!("Meet-in-the-Middle Attack (MITM)");
 
-    let h = vec![491];
-    let h = BigUint::new(h);
-    let g = vec![2];
-    let g = BigUint::new(g);
-    let p = vec![499];
-    let p = BigUint::new(p);
-    let b = 2u32.pow(3);
+    let h = BigUint::parse_bytes(b"3239475104050450443565264378728065788649097520952449527834792452971981976143292558073856937958553180532878928001494706097394108577585732452307673444020333", 10).unwrap();
+    let g = BigUint::parse_bytes(b"11717829880366207009516117596335367088558084999998952205599979459063929499736583746670572176471460312928594829675428279466566527115212748467589894601965568", 10).unwrap();
+    let p = BigUint::parse_bytes(b"13407807929942597099574024998205846127479365820592393377723561443721764030073546976801874298166903427690031858186486050853753882811946569946433649006084171", 10).unwrap();
+    let b = 2u32.pow(20);
 
     let table = build_table(&h, &g, &p, b);
     match lookup_x0_x1(&table, &g, &p, b) {
